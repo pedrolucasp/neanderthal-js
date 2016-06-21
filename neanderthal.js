42a91d1c76beae806ef0ccd9f2d42d0a5ae84ade
Pedro Lucas Porcellis <pedrolucasporcellis@gmail.com>
2016-06-20 21:44:26 -0300

    Scaffold files

---
+++ neanderthal.js
@@ -0,0 +1,5 @@
+'use strict';
+// Neanderthal JS
+// Pedro Lucas Porcellis - pedrolucasporcellis@gmail.com
+// 20/06/2016
+// MIT License (see LICENSE)
---
+++ test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Documentation Reference: https://gist.github.com/yoavniran/1e3b0162e1545055429e
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Example', function () {
+  it('should pass all tests', function () {
+    expect(true).to.equal(true);
+  });
+});
